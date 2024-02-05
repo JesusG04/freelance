480ff6427603f82a30a4f82c08dc639d00edf89b
JesusG04 <jesusgs67@outlook.com>
2024-02-05 17:41:47 -0600

    Integacion de sass y definicion de paleta de colores

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const{src, dest, watch, parallel} = require("gulp");
 
 const sass = require("gulp-sass")(require("sass"));
+const plumber= require('gulp-plumber');
 
 function css(done){
-    src('src/scss/app.scss') //identificamos los archivos
+    src('src/scss/**/*.scss') //identificamos los archivos
+    .pipe(plumber())
     .pipe(sass())   //compilamos los archivos
-    .on('error', sass.logError) // Agregamos manejo de errores
     .pipe(dest("build/css")); //*guardamos los archivos
 
     done();
 }
+function dev(done){
+    watch('src/scss/**/*.scss', css);
+    done();
+}
+
 
 exports.css = css;
+exports.dev = dev;
