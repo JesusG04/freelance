c58eee2cd70a049591b5933c08511ade13eabde1
JesusG04 <jesusgs67@outlook.com>
2024-02-05 19:15:31 -0600

    Se creo el header, la navegacion y las imganes se transformaron en diferentes formatos

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ const{src, dest, watch, parallel} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber= require('gulp-plumber');
 
+//imagenes
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+
+
 function css(done){
     src('src/scss/**/*.scss') //identificamos los archivos
     .pipe(plumber())
@@ -11,6 +18,42 @@ function css(done){
 
     done();
 }
+
+//Optimizar  imagenes
+function imagenes(done){
+    const opciones ={
+        optimizationLevel: 3
+    };
+   src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe(dest('build/img'));
+   done();
+}
+
+//Convertir imagenes formato webp
+function versionWebp(done){
+    const opciones = {
+        quality:50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+//Convertir imagenes formato avif
+function versionAvif(done){
+    const opciones = {
+        quality:50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+
+
+
+
 function dev(done){
     watch('src/scss/**/*.scss', css);
     done();
@@ -18,4 +61,7 @@ function dev(done){
 
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.imagenes = imagenes;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
